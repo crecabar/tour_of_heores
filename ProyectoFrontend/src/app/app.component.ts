ab0baf6036991383468f6695c31858960a70d22c
Cristian Recabarren <cristian.recabarren.madrid@gmail.com>
2023-07-06 15:58:18 -0400

    Angular, added features to consume api data

--- ProyectoFrontend/src/app/app.component.ts
+++ ProyectoFrontend/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProyectoFrontend';
+  title = 'Tour of Heroes';
+
+  apiBaseURL = 'http://localhost:5000'
 }
